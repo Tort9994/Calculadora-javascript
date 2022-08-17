df6d293fa1b3cde88eb63ebc1dc4b906f46e0047
Tort9994 <108017207+Tort9994@users.noreply.github.com>
2022-08-16 23:51:58 -0500

    subir archivos

---
+++ javascript/calculadora.js
@@ -0,0 +1,7 @@
+
+function suma(){
+var a = parseFloat(document.getElementById('a').value);
+var b = parseFloat(document.getElementById('b').value);
+var c = a + b; 
+document.getElementById('c').value = c;
+}
